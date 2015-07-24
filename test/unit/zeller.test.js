d0d47520e6adc5233001e8fec1b42e0864ee681f
calebgregory <calebgregory@gmail.com>
2015-07-23 20:09:05 -0500

    Adding tests and implementation for our Zeller congruence function

---
+++ test/unit/zeller.test.js
@@ -0,0 +1,25 @@
+var should = require('chai').should();
+var path = require('path');
+var zeller = require(path.join(process.cwd(),'/lib/zeller'));
+
+describe('Zeller', function() {
+
+  describe('#convertMonth()', function() {
+    it('should convert January to 13 and February to 14', function() {
+      zeller.convertMonth(1).should.equal(13);
+      zeller.convertMonth(2).should.equal(14);
+    });
+  });
+
+  describe('#yearOfTheCentury()', function() {
+    it('should return the year mod 100', function() {
+      zeller.yearOfTheCentury(2015).should.equal(15);
+    });
+  });
+
+  describe('#zeroBasedCentury()', function() {
+    it('should return the century our year is in - starting from 0', function() {
+      zeller.zeroBasedCentury(2015).should.equal(20);
+    });
+  });
+});
