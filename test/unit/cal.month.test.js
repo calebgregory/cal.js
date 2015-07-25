d57b884c7753779a033aba2e6c1ebec73f64a1bf
calebgregory <calebgregory@gmail.com>
2015-07-25 12:56:02 -0500

    Adding zeller module to month function to grab the weekday of the first day of a given month

--- test/unit/cal.month.test.js
+++ test/unit/cal.month.test.js
@@ -10,9 +10,22 @@ describe.only('Month', function() {
     });
   });
 
-  describe('#days', function() {
+  describe('#wkdys', function() {
     it('should return a list of the names of the days in a row', function() {
       month.wkdys.should.equal('Su Mo Tu We Th Fr Sa\n');
+    });
+  });
+
+  describe('#days()', function() {
+    it('should start with the first day of the month, and print a list of days', function() {
+      var output = [
+        '          1  2  3  4\n',
+        ' 5  6  7  8  9 10 11\n',
+        '12 13 14 15 16 17 18\n',
+        '19 20 21 22 23 24 25\n',
+        '26 27 28 29 30 31\n'
+      ].join('');
+      month.days(7,2015).should.equal(output);
     });
   });
 });
