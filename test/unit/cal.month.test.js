e07a97583b61ab204b07ed723d38e8ed1539d08f
calebgregory <calebgregory@gmail.com>
2015-07-25 12:23:43 -0500

    Adding display table with values for monthly display and tests for title and day list

--- test/unit/cal.month.test.js
+++ test/unit/cal.month.test.js
@@ -4,7 +4,15 @@ var should = require('chai').should();
 var month = require(path.join(process.cwd(),'/lib/cal.month'));
 
 describe('Month', function() {
-  it('should print an evenly spaced title with year name', function() {
-    month(title).should.equal('     July 2015');
+  describe('#title()', function() {
+    it('should print an evenly spaced title with year name', function() {
+       month.title(7).should.equal('     July 2015\n');
+    });
+  });
+
+  describe('#days', function() {
+    it('should return a list of the names of the days in a row', function() {
+      month.wkdys.should.equal('Su Mo Tu We Th Fr Sa');
+    });
   });
 });
