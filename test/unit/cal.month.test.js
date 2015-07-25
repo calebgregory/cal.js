581fd0df96917ee3c4682824b97272c3a31843d8
calebgregory <calebgregory@gmail.com>
2015-07-25 13:27:51 -0500

    Organizing tests to be alphabetical

--- test/unit/cal.month.test.js
+++ test/unit/cal.month.test.js
@@ -4,18 +4,6 @@ var should = require('chai').should();
 var month = require(path.join(process.cwd(),'/lib/cal.month'));
 
 describe.only('Month', function() {
-  describe('#title()', function() {
-    it('should print an evenly spaced title with year name', function() {
-       month.title(7).should.equal('     July 2015\n');
-    });
-  });
-
-  describe('#wkdys', function() {
-    it('should return a list of the names of the days in a row', function() {
-      month.wkdys.should.equal('Su Mo Tu We Th Fr Sa\n');
-    });
-  });
-
   describe('#days()', function() {
     it('should start with the first day of the month, and print a list of days', function() {
       var output = [
@@ -35,4 +23,17 @@ describe.only('Month', function() {
       month.startIndex(4).should.equal(11);
     });
   });
+
+  describe('#title()', function() {
+    it('should print an evenly spaced title with year name', function() {
+       month.title(7).should.equal('     July 2015\n');
+    });
+  });
+
+  describe('#wkdys', function() {
+    it('should return a list of the names of the days in a row', function() {
+      month.wkdys.should.equal('Su Mo Tu We Th Fr Sa\n');
+    });
+  });
+
 });
