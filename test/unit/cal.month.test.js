3613eb13cc9038214bf4503cddfcc2ac25108484
calebgregory <calebgregory@gmail.com>
2015-07-26 16:08:50 -0500

    Modifying month display to be adaptable to months with 5 or 6 rows and generate a compatible output for each

--- test/unit/cal.month.test.js
+++ test/unit/cal.month.test.js
@@ -60,6 +60,20 @@ describe('Month', function() {
       ].join('');
       month.display(1,2015).should.equal(output);
     });
+
+    it('should display a month with six weeks', function() {
+      var output = [
+        '    August 2015',
+        'Su Mo Tu We Th Fr Sa',
+        '                   1',
+        ' 2  3  4  5  6  7  8',
+        ' 9 10 11 12 13 14 15',
+        '16 17 18 19 20 21 22',
+        '23 24 25 26 27 28 29',
+        '30 31\n'
+      ].join('\n');
+      month.display(8,2015).should.equal(output);
+    });
   });
 
   describe('#startIndex()', function() {
