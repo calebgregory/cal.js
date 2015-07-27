783d828aee4297177787b6b34287b5eef1a77beb
calebgregory <calebgregory@gmail.com>
2015-07-27 12:01:13 -0500

    Adding 'today' module for calculating what day it is currently

---
+++ test/unit/cal.today.test.js
@@ -0,0 +1,25 @@
+var should = require('chai').should();
+var path = require('path');
+var year = require(path.join(process.cwd(),'/lib/cal.year'));
+
+describe.skip('Today', function() {
+  it('should know the current month', function() {
+    var today = require(path.join(process.cwd(),'/lib/cal.today'));
+    today.month.should.equal(7);
+  });
+
+  it('should know the current day', function() {
+    var today = require(path.join(process.cwd(),'/lib/cal.today'));
+    today.day.should.equal(23);
+  });
+
+  it('should know the current year', function() {
+    var today = require(path.join(process.cwd(),'/lib/cal.today'));
+    today.year.should.equal(2015);
+  });
+
+  it('should know what day of the week it is', function() {
+    var today = require(path.join(process.cwd(),'/lib/cal.today'));
+    today.weekday.should.equal('Thursday');
+  });
+});
